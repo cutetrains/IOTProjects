b0cf96239c9675ba0e05181e95d1051d869863b1
Gustaf Brännberg <gustaf_brannberg@hotmail.com>
2022-08-09 23:10:41 +0200

    Accept phone number and message as parameters

--- misc/sendSMS.py
+++ misc/sendSMS.py
@@ -1,20 +1,31 @@
 import subprocess
-
-#https://github.com/plewin/tp-link-modem-router
-
-#Script not tested on RPI on network with TP-link 4G router yet
 import os
 from os.path import expanduser
 from dotenv import load_dotenv
+import sys
+#https://github.com/plewin/tp-link-modem-router
+
+#Script not tested on RPI on network with TP-link 4G router yet
 
 load_dotenv(expanduser("~")+'\.env')
-
 ip_TPLink = os.getenv('ip_TPLink')
 user_TPLink = os.getenv('user_TPLink')
 password_TPLink = os.getenv('password_TPLink')
-phone_number = os.getenv('phone_number')
 
-message = "Stream started from RPI. URL: https://www.youtube.com/channel/UC1lDhijDvNyHWC1y4KuR0Iw/live"
+if(len(sys.argv) == 1):
+    print("No parameters specified, using phone number from .env and test2 as message.")
+    phone_number = os.getenv('phone_number')
+    message = "test2"
+
+elif(len(sys.argv) == 3):
+    phone_number = sys.argv[1]
+    message  = sys.argv[2]
+else:
+     print("Usage:")
+     print("python sendSMS.py")
+     print('python sendSMS.py 01234567890 "Lorem ipsum dolor sit amet"')
+     sys.exit()
+#message = "Stream started from RPI. URL: https://www.youtube.com/channel/UC1lDhijDvNyHWC1y4KuR0Iw/live"
 
 print(f'cd /var/www/html/plugins/script/data/tp-link-modem-router/; ./sms-send.js --url="http://{ip_TPLink}" --login="{user_TPLink}" --password="{password_TPLink}" "{phone_number}" "{message}"; cd ~/sandbox/ ')
 
@@ -22,4 +33,3 @@ cmdList = ['cd', '/var/www/html/plugins/script/data/tp-link-modem-router/;', './
 print(cmdList)
 
 s=subprocess.call(cmdList)
-
